4c2cfef8d28cbc537b63594f1480fcbceaa22ee0
Staselko Natalia <nataliastaselko8@gmail.com>
2026-05-20 13:24:05 +0300

    add cache

--- src/index.js
+++ src/index.js
@@ -2,43 +2,40 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url)
 
-    // CORS headers
     const corsHeaders = {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type"
     }
 
-    // preflight request
     if (request.method === "OPTIONS") {
-      return new Response(null, {
-        headers: corsHeaders
-      })
+      return new Response(null, { headers: corsHeaders })
     }
 
     if (url.pathname === "/api/locale") {
       const lang = url.searchParams.get("lang") || "en"
 
-      const data = await env.I18N.get(lang)
+      const cache = caches.default
+      const cached = await cache.match(request)
 
-      if (!data) {
-        return new Response(JSON.stringify({ error: "not found" }), {
-          status: 404,
-          headers: {
-            "Content-Type": "application/json",
-            ...corsHeaders
-          }
-        })
+      if (cached) {
+        return cached
       }
 
-      return new Response(data, {
+      const data = await env.I18N.get(lang) || await env.I18N.get("en")
+
+      const response = new Response(data, {
         headers: {
           "Content-Type": "application/json",
-          //"Cache-Control": "public, max-age=3600",
-          "Cache-Control": "no-store",
+          "Cache-Control": "public, max-age=60", // короткий кеш
           ...corsHeaders
         }
       })
+
+      // кладём в edge cache
+      await cache.put(request, response.clone())
+
+      return response
     }
 
     return new Response("OK", { headers: corsHeaders })
