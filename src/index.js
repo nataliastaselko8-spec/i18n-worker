e45f37a8569347a0aa16d4f3b3219085699be830
Staselko Natalia <nataliastaselko8@gmail.com>
2026-05-20 12:46:30 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response("Hello World!");
-	},
-};
+  async fetch(request, env) {
+    const url = new URL(request.url)
+
+    // API перевода
+    if (url.pathname === "/api/locale") {
+      const lang = url.searchParams.get("lang") || "en"
+
+      const data = await env.I18N.get(lang)
+
+      if (!data) {
+        return new Response(
+          JSON.stringify({ error: "not found" }),
+          {
+            status: 404,
+            headers: {
+              "Content-Type": "application/json"
+            }
+          }
+        )
+      }
+
+      return new Response(data, {
+        headers: {
+          "Content-Type": "application/json",
+          "Cache-Control": "public, max-age=3600"
+        }
+      })
+    }
+
+    return new Response("OK")
+  }
+}
